9a4a5f43c4a30003bb38f87ced470189aab81696
Tekuri Sai Akhil <st5050@nyu.edu>
2024-05-28 19:58:03 -0400

    Integrate frontend & backend

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from fastapi.middleware.cors import CORSMiddleware
+
+class VideoAnalysisRequest(BaseModel):
+    youtube_link: HttpUrl
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_headers = ["*"],
+    allow_methods = ["*"]
+)
+
+@app.post("/analyze_video")
+def analyze_video(request: VideoAnalysisRequest):
+    from langchain_community.document_loaders import YoutubeLoader
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+    loader  = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
+    docs = loader.load()
+    print(f"{type(docs)}")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=0)
+    result = text_splitter.split_documents(docs)
+    print(f"{type(result)}")
+
+    author = result[0].metadata['author']
+    length = result[0].metadata['length']
+    title = result[0].metadata['title']
+    total_size = len(result)
+
+    return {
+        "author" : author,
+        "length":length,
+        "title": title,
+        "total_size": total_size,
+        "youtube_link" : request.youtube_link
+    }
+
+@app.get("/root")
+def health():
+    return {"status": "ok"}
